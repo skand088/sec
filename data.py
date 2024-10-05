afb41ac85d3a890af2ba6b2f8405284f5813d01f
abia-jahangir <abiajahangir1@gmail.com>
2024-10-05 12:54:51 -0600

    yellow

--- data.py
+++ data.py
@@ -10,3 +10,16 @@ times = pd.DataFrame({'time': ['8:00','8:15','8:30']})
 
 doctors = pd.DataFrame({'speciality': ['Dr.A', 'Dr.B', 'Dr.C', 'Dr.D', 'Dr.E', 'Dr.F'],'specialty': ['cardiology', 'cardiology', 'dermatologist', 'family doctor', 'pediatrician', 'gynecologist']})
 specialities = ['']
+
+class Doctor:
+    def __init__(self, doctor_id, doctor_name, specialty, available):
+        self.doctor_id = doctor_id
+        self.doctor_name = doctor_name
+        self.specialty = specialty
+        self.available = available
+    
+    def specialty(self):
+        return self.specialty
+    
+if __name__ == '__main__':
+    
